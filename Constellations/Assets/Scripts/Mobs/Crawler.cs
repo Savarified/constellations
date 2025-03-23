79951b810fdd5f6916e52b759da8582efd89ce72
offlinepls <selfmadeprocrastinator@proton.me>
2025-03-23 01:57:42 +0100

    Reapply "Merge branch 'main' of https://github.com/Savarified/constellations"
    This reverts commit 6e8cbafd1e6cca2fa77aa4f18a5f412ffbf94427.

---
+++ Constellations/Assets/Scripts/Mobs/Crawler.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class Crawler : MonoBehaviour
+{
+    [Header("AI Settings")]
+    [SerializeField] private float sight_range, attack_range;
+    [SerializeField] private int damage;
+    [SerializeField] private Animator anim;
+    [SerializeField] private float time_to_attack, time_to_patrol;
+    private float attack_tick, patrol_tick;
+    public GameObject player;
+    private bool moving, attacking;
+    [Header("Movement Settings")]
+    [SerializeField] private float move_speed;
+    private Vector2 patrol_target;
+    void Awake(){
+        anim = gameObject.GetComponent<Animator>();
+    }
+    void FixedUpdate(){
+        float dist = Vector2.Distance(player.transform.position, transform.position);
+        if (dist <= attack_range){
+            attacking = true;
+        }
+        else if(dist <= sight_range){
+            attacking = false;
+            moving = true;
+        }
+        else{
+            moving = false;
+            patrol_tick += Time.deltaTime;
+            if (patrol_tick >=  time_to_patrol){
+                patrol_tick = 0f;
+                time_to_patrol = Random.Range(0.5f, 5f);
+                patrol_target = Random.insideUnitCircle * 7f;
+            }
+            transform.position = Vector2.MoveTowards(transform.position, patrol_target, move_speed * Time.deltaTime);
+        }
+
+        anim.SetBool("move", true);
+
+        if (moving){
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, move_speed * Time.deltaTime);
+        }
+
+        if (attacking){
+            attack_tick += Time.deltaTime;
+            if (attack_tick >= time_to_attack){
+                attack_tick = 0f;
+                Attack();
+            }
+        }
+
+    }
+
+    void Attack()
+    {
+        //player.GetComponent<PlayerStats>().health -= damage; not implemented yet
+        anim.SetTrigger("bite");
+    }
+
+    void SetNewDestination(){
+
+    }
+}
